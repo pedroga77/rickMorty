c17f53246e1180bac695ed8529f7877416b82cb5
GUSTAVO MIGUEL DOS SANTOS PEREIRA <24122458@dsn127m1246728.cfp127aluno.local>
2025-05-15 09:39:55 -0300

    ok

---
+++ app/src/main/java/com/example/rickandmorty/model/Character.kt
@@ -0,0 +1,9 @@
+package com.example.rickandmorty.model
+
+data class Character(
+    var id: Int = 0,
+    var name: String = "",
+    var species: String = "",
+    var status: String = "",
+    var image: String = ""
+)
